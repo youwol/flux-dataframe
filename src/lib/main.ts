20bc3b6cfa213caeb0f567e0bfba9df5a736667d
greinisch <greinisch@youwol.com>
2021-05-19 12:57:23 +0200

    :sparkles: dataframe journal view

--- src/lib/main.ts
+++ src/lib/main.ts
@@ -1,7 +1,21 @@
-import { FluxPack, IEnvironment } from '@youwol/flux-core'
+import { DataFrame, IArray, Serie } from '@youwol/dataframe'
+import { FluxPack, IEnvironment, Journal } from '@youwol/flux-core'
 import { AUTO_GENERATED } from '../auto_generated'
+import { dataframeJournalView } from './dataframe.view'
 
 export function install(environment: IEnvironment){
+    Journal.registerView({
+        name:'Dataframe @ flux-dataframe',
+        description:'Journal view to display dataframe',
+        isCompatible: (data:unknown) => {
+            let ok = data instanceof DataFrame || data instanceof Serie
+            return ok
+        },
+        view: (df: DataFrame | Serie<IArray>) => {
+
+            return dataframeJournalView(df) as any
+        }
+    })
     return
 }
 
--- src/lib/tables-view.module.ts
+++ src/lib/tables-view.module.ts
@@ -8,6 +8,7 @@ import { DataFrame, IArray, Serie } from '@youwol/dataframe'
 import { pack } from './main'
 import { BehaviorSubject, combineLatest } from 'rxjs'
 import { map } from 'rxjs/operators'
+import { dataFrameView } from './dataframe.view'
 
 /**
   ## Presentation
@@ -132,121 +133,6 @@ export namespace ModuleTablesView {
         return { innerText: tab.name , class: 'px-2'}
     }
 
-    function optionsView(
-        startIndex$: BehaviorSubject<number>, 
-        bufferSize$:  BehaviorSubject<number>
-        ): VirtualDOM{
-        
-        let elem = (subject$, title) => {
-            return {
-                class: 'd-flex p-2 align-items-center',
-                children: [
-                    {
-                        innerText: title
-                    },
-                    {
-                        tag: 'input',
-                        class: 'mx-2',
-                        type: 'number',
-                        value: subject$.getValue(),
-                        onchange: (ev) => subject$.next(parseInt(ev.target.value))
-                    }
-                ]
-            }
-        }
-        return {
-            class: 'd-flex p-2 align-items-center',
-            children: [
-                elem(startIndex$, 'start'),
-                elem(bufferSize$, 'buffer'),
-            ]
-        }
-    }
-
-    function cellView(data: IArray, options){
-
-        return { 
-            tag: 'td', 
-            class: 'px-3',
-            children:[{
-                class:'d-flex',
-                children: data.map( d => ({ 
-                    class: 'px-1',
-                    style:{'white-space': 'nowrap'},
-                    innerText: d.toExponential(2)}) ),
-            }]
-        }
-    }
-
-    function dataFrameView(df: DataFrame): VirtualDOM {
-
-        let startIndex$ = new BehaviorSubject(0)
-        let bufferSize$ = new BehaviorSubject(100)
-        
-        let columns = Array.from(df.series.keys())
-        let chunk$ = combineLatest([startIndex$, bufferSize$]).pipe(
-            map(([startIndex, bufferSize]) => {
-                
-                let series : Serie<IArray>[]= columns.reduce((acc, name) => [...acc, df.get(name)], [])
-                let totalRowCount = series[0].length / series[0].itemSize
-                if(startIndex+bufferSize > totalRowCount)
-                    bufferSize = totalRowCount - startIndex
-
-                let chunk = [...new Array(bufferSize)].map((_, rowIndex) => {
-                    let values = series.map((serie: Serie<IArray>) => serie.itemAt(startIndex + rowIndex))
-                    return [ [startIndex + rowIndex], ...values]
-                })
-                return chunk
-            })
-        )
-        return {
-            class: 'd-flex flex-column h-100 fv-bg-background fv-text-primary',
-            children: [
-                optionsView(startIndex$, bufferSize$),
-                {
-                    class: 'flex-grow-1 overflow-auto w-100', style: { 'min-height': '0px' },
-                    children: [
-                        {
-                            tag: 'table',
-                            class: 'fv-color-primary text-center h-100 w-100 fv-text-primary',
-                            children: [
-                                {
-                                    tag: 'thead',
-                                    children: [
-                                        {
-                                            tag: 'tr', class: 'fv-bg-background-alt',
-                                            children: [
-                                                { tag: 'td', innerText: '', class: 'px-2' },
-                                                ...columns.map((col: string) => {
-                                                    return { tag: 'td', innerText: col, class: 'px-2' }
-                                                })]
-                                        }
-                                    ]
-                                },
-                                child$(
-                                    chunk$,
-                                    (chunk) => {
-                                        return {
-                                            tag: 'tbody',
-                                            children: chunk.map(row => {
-
-                                                return {
-                                                    tag: 'tr',
-                                                    class: 'fv-hover-bg-background-alt',
-                                                    children: row.map((data) => cellView(data, {}))
-                                                }
-                                            })
-                                        }
-                                    }
-                                )
-                            ]
-                        }
-                    ]
-
-                }
-            ]
-        }
-    }
 
     function renderHtmlElement(mdle: Module) {
 
