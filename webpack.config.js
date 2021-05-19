d9bfc56f10bc06227d4d4f25a134e443ea109c42
greinisch <greinisch@youwol.com>
2021-05-19 12:58:03 +0200

    :bug: webpack.config=>missing @youwol/dataframe external

--- webpack.config.js
+++ webpack.config.js
@@ -34,6 +34,7 @@ module.exports = {
         '@youwol/flux-core': "@youwol/flux-core",
         '@youwol/cdn-client': '@youwol/cdn-client',
         '@youwol/flux-view': "@youwol/flux-view",
+        "@youwol/dataframe": "@youwol/dataframe",
     }],
     module: {
         rules: [
