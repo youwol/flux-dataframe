ad700899ce275e8488786c9bcdd1ebb4ed09b7f2
greinisch <greinisch@youwol.com>
2021-07-27 11:07:07 +0200

    :zap: add auto-generated file in bundle

---
+++ src/auto_generated.ts
@@ -0,0 +1,6 @@
+export let AUTO_GENERATED = {
+    name: "@youwol/flux-dataframe",
+    namespace: "youwol",
+    version: "0.0.4",
+    description: "Collection of modules for Flux to create & manipulate data-frames"
+}
