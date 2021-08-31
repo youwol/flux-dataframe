038075078d29b4a3a348889e6153ed311d81928d
greinisch <greinisch@youwol.com>
2021-08-31 11:04:10 +0200

    :bookmark: 0.0.5

--- src/auto_generated.ts
+++ src/auto_generated.ts
@@ -1,6 +1,6 @@
 export let AUTO_GENERATED = {
     name: "@youwol/flux-dataframe",
     namespace: "youwol",
-    version: "0.0.4",
+    version: "0.0.5",
     description: "Collection of modules for Flux to create & manipulate data-frames"
 }
